3d2e7da6d1017f8c1c59fcd6abc89dacceb5d8cf
nghaika01 <mykenjoroge01@gmail.com>
2022-06-21 19:54:42 +0300

    Added custom theme

--- src/utils/theme.jsx
+++ src/utils/theme.jsx
@@ -3,6 +3,10 @@ import { createTheme } from "@mui/material";
 const darkTheme = createTheme({
   palette: {
     mode: "light",
+    primary: { main: "#7b1fa2" },
+    secondary: {
+      main: "#212121",
+    },
   },
 });
 
