c4c752eabced0c0cf61a5431868f3ee487529c76
nghaika01 <mykenjoroge01@gmail.com>
2022-06-21 21:26:02 +0300

    added admin routes

--- src/app/routes/protected-routes.js
+++ src/app/routes/protected-routes.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CreateProduct from "../../pages/admin/CreateProduct";
+import ListProducts from "../../pages/admin/ListProducts";
 import LoginScreen from "../../pages/auth/Login";
 import RegisterScreen from "../../pages/auth/Register";
 import Home from "../../pages/Home";
@@ -10,6 +12,8 @@ const ProtectedRoutes = () => {
         <Routes>
           <Route path="/" element={<LoginScreen />} />
           <Route path="/signup" element={<RegisterScreen />} />
+          <Route path="/admin/products" element={<ListProducts />} />
+          <Route path="/admin/products/add" element={<CreateProduct />} />
           <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,8 @@ const Home = () => {
     <>
       <NavBar />
       
+
+      
     </>
   );
 };
