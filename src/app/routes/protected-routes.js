53a2336e6a31058791cb7d33a3542072be088ad0
nghaika01 <mykenjoroge01@gmail.com>
2022-06-25 10:38:49 +0300

    added orders routes

--- src/app/routes/protected-routes.js
+++ src/app/routes/protected-routes.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CreateProduct from "../../pages/admin/CreateProduct";
+import EditProduct from "../../pages/admin/EditProduct";
 import ListProducts from "../../pages/admin/ListProducts";
 import LoginScreen from "../../pages/auth/Login";
 import RegisterScreen from "../../pages/auth/Register";
 import Cart from "../../pages/cart/Cart";
 import CheckOut from "../../pages/checkout/CheckOut";
 import Home from "../../pages/Home";
+import MyOrders from "../../pages/orders/MyOrders";
+import Orders from "../../pages/orders/Orders";
 import ProductDetail from "../../pages/products/ProductDetail";
 
 const ProtectedRoutes = () => {
@@ -19,7 +22,10 @@ const ProtectedRoutes = () => {
           <Route path="/products/:id" element={<ProductDetail />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<CheckOut />} />
+          <Route path="/orders" element={<MyOrders />} />
+          <Route path="/admin/orders" element={<Orders />} />
           <Route path="/admin/products/add" element={<CreateProduct />} />
+          <Route path="/admin/products/:id" element={<EditProduct />} />
           <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
